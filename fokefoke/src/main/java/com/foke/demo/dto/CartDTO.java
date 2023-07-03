3e757d73a0e8a6f894a19e64527ab3c4f1c077ec
tjoeun_jg <tjoeun_jg@DESKTOP-D045G56>
2023-07-03 11:35:18 +0900

    cart업데이트

--- fokefoke/src/main/java/com/foke/demo/dto/CartDTO.java
+++ fokefoke/src/main/java/com/foke/demo/dto/CartDTO.java
@@ -1,19 +1,29 @@
 package com.foke.demo.dto;
 
+import java.io.Serializable;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "cart")
 @Entity
-public class CartDTO {
+@Builder
+public class CartDTO implements Serializable{
+	
+	private static final long serialVersionUID = 1L; //클래스를 구분하는데 사용(CartDTO 데이터를 전송하거나 저장할 때 발생문제 해결)
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -65,23 +75,23 @@ public class CartDTO {
 	private String extramain;
 	
 	@Column(length = 100, nullable = false)
-	private String setbeverage;
+	private String setbeverage = "";
 	
 	@Column(length = 100, nullable = false)
 	private String setside;
 	
-	@Column
-	private int total;
-	
-	@Column
-	private int price;
+	@Column(nullable = false)
+	private Integer total;
 	
 	@Column(nullable = false)
-	private int onePrice;
+	private Integer price;
 	
-	//member테이블
 	@Column(nullable = false)
-	private int point;
+	private Integer onePrice;
+	
+	//추가(한개의 포인트)
+	@Column(nullable = false)
+	private Integer point;
 	
 	//store 테이블
 	@Column(length = 100, nullable = false)
@@ -91,21 +101,33 @@ public class CartDTO {
 	private String storeAddress;
 	
 	//추가(가격)
-	@Column
-	private int totalPrice; //(+배송비)
+	@Column(nullable = false)
+	private Integer totalPrice; //(+배송비)
 
 	//추가(포인트)
-	@Column
-	private int totalPoint;
+	@Column(nullable = false)
+	private Integer totalPoint;
 	
 	//추가(상품이미지)
 	@Column(length = 100, nullable = false)
 	private String cartImage;
 	
 	public void initTotal() {
-		//this.cartCount = this.total;
 		this.totalPrice = this.onePrice * this.cartCount;
 		this.point = (int)(Math.floor(this.onePrice*0.05));
 		this.totalPoint = this.point * this.cartCount;
 	}
+
+	@Override
+	public String toString() {
+		return "CartDTO [cartId=" + cartId + ", memberId=" + memberId + ", fokeingredientId=" + fokeingredientId
+				+ ", storeId=" + storeId + ", cartCount=" + cartCount + ", productName=" + productName + ", base="
+				+ base + ", atopping=" + atopping + ", btopping=" + btopping + ", ctopping=" + ctopping + ", dtopping="
+				+ dtopping + ", asource=" + asource + ", bsource=" + bsource + ", aextratopping=" + aextratopping
+				+ ", bextratopping=" + bextratopping + ", extramain=" + extramain + ", setbeverage=" + setbeverage
+				+ ", setside=" + setside + ", total=" + total + ", price=" + price + ", onePrice=" + onePrice
+				+ ", point=" + point + ", storeName=" + storeName + ", storeAddress=" + storeAddress + ", totalPrice="
+				+ totalPrice + ", totalPoint=" + totalPoint + ", cartImage=" + cartImage + "]";
+	}
+
 }
