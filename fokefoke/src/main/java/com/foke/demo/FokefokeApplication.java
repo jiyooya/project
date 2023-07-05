6758a7a920c8beb68ae0f973040c1d657ea4cb74
지환 <whgdmsdpseld@naver.com>
2023-07-05 12:18:21 +0900

    어플리케이션 수정

--- fokefoke/src/main/java/com/foke/demo/FokefokeApplication.java
+++ fokefoke/src/main/java/com/foke/demo/FokefokeApplication.java
@@ -2,8 +2,10 @@ package com.foke.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.foke.demo")
 public class FokefokeApplication {
 
 	public static void main(String[] args) {
